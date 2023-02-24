877eb682c11435d09395bb042cb469d90bd900cd
Sergio <sergiobluez@gmail.com>
2023-02-24 12:57:07 -0800

    Budget app with styling in React
    Basic budget app in React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [expenses, setExpenses] = useState([]);
+
+  function addExpense(event) {
+    event.preventDefault();
+    const name = event.target.elements.name.value;
+    const amount = event.target.elements.amount.value;
+    const date = event.target.elements.date.value;
+    const newExpense = { name, amount, date };
+    setExpenses([...expenses, newExpense]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Budget App</h1>
+      <form onSubmit={addExpense}>
+        <label>Name:</label>
+        <input type="text" name="name" />
+        <label>Amount:</label>
+        <input type="number" name="amount" />
+        <label>Date:</label>
+        <input type="date" name="date" />
+        <button>Add expense</button>
+      </form>
+      <ul>
+        {expenses.map((expense, index) => (
+          <li key={index}>
+            <div>Name: {expense.name}</div>
+            <div>Amount: {expense.amount}</div>
+            <div>Date: {expense.date}</div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
